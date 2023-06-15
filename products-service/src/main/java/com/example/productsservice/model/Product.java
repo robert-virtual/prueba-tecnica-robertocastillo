9b7a430ad973c664b5c4e1ad57e5682746ee669a
robert-virtual <robertocastill63@gmail.com>
2023-06-14 23:44:50 -0600

    add product model properties

--- products-service/src/main/java/com/example/productsservice/model/Product.java
+++ products-service/src/main/java/com/example/productsservice/model/Product.java
@@ -4,5 +4,12 @@ import lombok.Data;
 
 @Data
 public class Product {
+    private long id;
+    private String title;
+    private double price;
+    private String description;
+    private String category;
+    private String image;
+    private Rating rating;
 
 }
---
+++ products-service/src/main/java/com/example/productsservice/model/Rating.java
@@ -0,0 +1,10 @@
+package com.example.productsservice.model;
+
+import lombok.Data;
+
+@Data
+public class Rating {
+
+    private float rate;
+    private int count;
+}
