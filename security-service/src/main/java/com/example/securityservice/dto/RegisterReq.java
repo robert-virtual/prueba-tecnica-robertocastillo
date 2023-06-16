69e913448e02256ada50264fe508ca7733e18a36
robert-virtual <robertocastill63@gmail.com>
2023-06-16 11:22:58 -0600

    add validations to register

--- security-service/src/main/java/com/example/securityservice/dto/RegisterReq.java
+++ security-service/src/main/java/com/example/securityservice/dto/RegisterReq.java
@@ -1,14 +1,25 @@
 package com.example.securityservice.dto;
 
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RegisterReq {
-    @NotNull(message = "password is mandatory")
+    @NotNull(message = "name is mandatory")
+    @NotEmpty(message = "name is mandatory")
     private String name;
+
+    @NotNull(message = "lastname is mandatory")
+    @NotEmpty(message = "lastname is mandatory")
     private String lastname;
+
+    @NotNull(message = "email is mandatory")
+    @NotEmpty(message = "email is mandatory")
     private String email;
+
+    @NotNull(message = "password is mandatory")
+    @NotEmpty(message = "password is mandatory")
     private String password;
 }
