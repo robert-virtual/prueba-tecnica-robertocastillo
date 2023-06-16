0f78735002508ed587e53615f40d07b7869d3511
robert-virtual <robertocastill63@gmail.com>
2023-06-16 11:25:09 -0600

    change empty fields message

--- security-service/src/main/java/com/example/securityservice/dto/RegisterReq.java
+++ security-service/src/main/java/com/example/securityservice/dto/RegisterReq.java
@@ -8,18 +8,18 @@ import lombok.Data;
 @Data
 public class RegisterReq {
     @NotNull(message = "name is mandatory")
-    @NotEmpty(message = "name is mandatory")
+    @NotEmpty(message = "name can not be empty")
     private String name;
 
     @NotNull(message = "lastname is mandatory")
-    @NotEmpty(message = "lastname is mandatory")
+    @NotEmpty(message = "lastname can not be empty")
     private String lastname;
 
     @NotNull(message = "email is mandatory")
-    @NotEmpty(message = "email is mandatory")
+    @NotEmpty(message = "email can not be empty")
     private String email;
 
     @NotNull(message = "password is mandatory")
-    @NotEmpty(message = "password is mandatory")
+    @NotEmpty(message = "password can not be empty")
     private String password;
 }
